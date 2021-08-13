ef17bdfab8dafcf1b3ce2ebe1034abc2704faab5
Cynthia Fu <yinnycfu@gmail.com>
2021-08-13 12:34:17 +0100

    chore: make restaurants route use the controller file

--- backend/api/restaurants.route.js
+++ backend/api/restaurants.route.js
@@ -1,11 +1,13 @@
 // this is a routes file
 
 import express from "express"
+// make this route file use the controller file
+import RestaurantsCtrl from "./restaurants.controller.js"
 
 // get access to the express router because we're going to be creating the different routes that people can go to
 const router = express.Router()
 
-// one demo route "/", it's going to respond with hello world.
-router.route("/").get((req, res) => res.send("hello world"))
+// one demo route "/", it's going to get the restaurants API
+router.route("/").get(RestaurantsCtrl.apiGetRestaurants)
 
 export default router
